6cc3b85f06395ec7bcd42c5dffc4d1d617511319
Anas <anasyousif182@gmail.com>
2026-05-17 05:08:05 -0400

    added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -10,16 +10,16 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
     def test_eq_with_url(self):
         node = TextNode("Link node", TextType.LINK,"https://www.boot.dev")
-        node2 = TextNode("Link node", TextType.LINK,"https://www.google.com")
+        node2 = TextNode("Link node", TextType.LINK,"https://www.boot.dev")
         self.assertEqual(node,node2)
     def test_not_eq_text_type(self):
         node = TextNode("This is text",TextType.BOLD)
-        node = TextNode("This is text",TextType.ITALIC)
-        self.assertEqual(node,node2)
+        node2 = TextNode("This is text",TextType.ITALIC)
+        self.assertNotEqual(node,node2)
     def test_not_eq_url(self):
         node = TextNode("Link node", TextType.LINK, "https://www.goole.com")
         node2 = TextNode("Link node",TextType.LINK, "https://www.boot.dev")
-        self.assertEqual(node,node2)
+        self.assertNotEqual(node,node2)
     def test_repr(self):
         node = TextNode("Text", TextType.CODE)
         self.assertEqual(repr(node),"TextNode(Test, code, None)")
